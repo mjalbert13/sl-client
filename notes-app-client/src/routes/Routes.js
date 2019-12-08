9e08611214d96ae279ea35a7cbbc030f7e7e80c1
mjalbert13 <mjalbert13@gmail.com>
2019-12-08 12:57:58 -0500

    update and delete working

--- notes-app-client/src/routes/Routes.js
+++ notes-app-client/src/routes/Routes.js
@@ -7,6 +7,8 @@ import Signup from '../pages/Signup';
 import Settings from '../pages/Settings';
 import NotFound from '../pages/NotFound';
 import NewNote from '../pages/Notes';
+import Notes from '../pages/Note';
+
 
 const Routes = ({ appProps}) => {
     return (
@@ -17,6 +19,7 @@ const Routes = ({ appProps}) => {
                 <AppliedRoute exact path='/login' component={Login} appProps={appProps} />
                 <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
                 <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+                <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
                 <Route exact path ='/settings'component={Settings} />
                 
                 <Route component={NotFound} />
